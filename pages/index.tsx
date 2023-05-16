44a606ef3121c8bf760d2f9f61a45c65349257f3
Hydra <115867087+IrorI-166@users.noreply.github.com>
2023-05-17 00:32:54 +0900

    Change a flame of directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Index_Header from './components/Index_Header'
+import Index_Header from '@/components/Index_Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
