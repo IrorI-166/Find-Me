9b49246cbcecd0e2508d7f9932127c1accfb600a
IrorI <115867087+IrorI-166@users.noreply.github.com>
2023-05-19 14:29:50 +0900

    Update TT_set.tsx

--- components/card/TT_set.tsx
+++ components/card/TT_set.tsx
@@ -2,7 +2,7 @@ import { Tooltip } from "@nextui-org/react";
 import { TooltipContent } from "./TooltipContent";
 import data from "@/testdata.json";
 
-export function TT_set({ children }) {
+export function TT_set({ children }: {children: React.ReactNode}) {
     return (
         <Tooltip
             content={<TooltipContent title={data.username1.title} description={data.username1.description} />}
